80353f74a85bb3c8fb004961bb48e918b49dd280
Brendan M. Unikewicz <123433613+zcwknu@users.noreply.github.com>
2026-05-12 08:36:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // ---------------------------------------------------------------------------
 // Paste the deployed Google Apps Script Web App URL between the quotes below.
 // (See README.md → "Newsletter signup" for the one-time setup.)
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwpKGNje7Wpdbt5OWeGBkrjU7v2OxrpMFxM018dVUVjM3_MdRa6f_rTOyt1W9gex4FH/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzhD-ScGMEiIMFA6eLSo-AM42gEXgNrxmXo1G2uAhKwhr45Xp53Uo0YWDwT9O5OcDuj/exec";
 // ---------------------------------------------------------------------------
 
 (function () {
